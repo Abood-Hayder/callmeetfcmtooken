e359238b79a5449e4a5ac0c9e419fab3b42ac6ce
Abood-Hayder <71792492+Abood-Hayder@users.noreply.github.com>
2025-04-19 17:13:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const admin = require('firebase-admin');
+
 admin.initializeApp({
-  credential: admin.credential.cert(require('./serviceAccountKey.json'))
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
+
 
 
 app.use(bodyParser.json());
