bcc54dee9f7bf47c6219f4c41bff99ffc0cefb06
Abood-Hayder <71792492+Abood-Hayder@users.noreply.github.com>
2025-04-20 03:27:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const { initializeApp } = require('firebase-admin/app');
+const app = initializeApp();
 
 const serviceAccount = require('./call-f258a-72f7060331c6.json');
 
